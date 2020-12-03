70ee78cf03525013535dc5dd432c8ef98d875ebb
baekCode <baekcode@gmail.com>
2020-12-03 22:23:23 +0900

    feature : Make Gradient

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
+import './app.css';
 
 function App() {
+    const [color, setColor] = useState({
+        startColor: '',
+        endColor: ''
+    })
+    const rectRef = useRef<HTMLDivElement>(null);
+    const onClick = ():void => {
+        const randomNum = Math.trunc(Math.random() * 100);
+        const randomHue = Math.trunc(Math.random() * 360);
+        const randomColorStart = `hsl(${randomHue}, 100%, 50%)`;
+        const randomColorEnd = `hsl(${randomHue + randomNum}, 100%, 50%)`;
+
+        const node = rectRef.current as HTMLDivElement
+        node.style.setProperty('--start', randomColorStart)
+        node.style.setProperty('--end', randomColorEnd)
+    }
     return (
-        <div>App</div>
+        <>
+            <h1>Make Gradient</h1>
+            <button onClick={onClick}>색상변경</button>
+            <div className="rectangle" ref={rectRef}></div>
+        </>
     );
 }
 
